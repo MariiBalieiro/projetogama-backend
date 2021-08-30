e6c3cba2784fd44e179c3bd2c88a8d34d238058d
Mariana Balieiro <marianabalieiro123@hotmail.com>
2021-08-29 23:01:34 -0300

    dotenv

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const swaggerDocs = require('./backend/swagger.json');
 const routes = require('./backend/routes');
 const app = express();
 const cors =  require('cors');
+require('dotenv').config({  
+    path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
+})
 
 mongoose.connect('mongodb+srv://bancodecurriculos:bancodecurriculos@cluster0.g4rth.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
