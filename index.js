dc8e07d6e2db155a72503b168ff70b79dc55c6be
Mariana Balieiro <marianabalieiro123@hotmail.com>
2021-08-29 22:07:03 -0300

    tentando contornar erro heroku

--- index.js
+++ index.js
@@ -16,4 +16,13 @@ app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(require('./backend/routes'));
 app.listen(process.env.PORT || '2800')
 
-
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    //Permitir headers fora do comum
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Custom-Header');
+    app.use(cors());
+    next();
+})
